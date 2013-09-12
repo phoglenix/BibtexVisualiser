7249a51955a74acfa247ffcf56bb03485721ece1
phoglenix <phoglenix@gmail.com>
2013-09-12 19:08:04 +1200

    generate pdf instead of png

--- run-graphvis.bat
+++ run-graphvis.bat
@@ -1,4 +1,5 @@
 @echo off
 echo Make sure you have python 2 and graphvis installed (put graphvis path in this file).
 python visualise.py
-"C:\Program Files (x86)\Graphviz 2.28\bin\neato.exe" -Gcharset=latin1 -Gepsilon=.001 -Tpng graph.dot -o graph.png
+"C:\Program Files (x86)\Graphviz 2.28\bin\neato.exe" -Gcharset=latin1 -Gepsilon=.001 -Goverlap=false -Tpng graph.dot -o graph.png
+"C:\Program Files (x86)\Graphviz 2.28\bin\neato.exe" -Gcharset=latin1 -Gepsilon=.001 -Goverlap=false -Tpdf graph.dot -o graph.pdf
