24c337e65ad739d7a95da93a973abbffb5bec593
phoglenix <phoglenix@gmail.com>
2013-09-12 17:50:49 +1200

    ignore outputs and other files, clarify batch file

--- run-graphvis.bat
+++ run-graphvis.bat
@@ -1,4 +1,4 @@
 @echo off
-echo Make sure you have python and graphvis installed (put graphvis path in this file).
+echo Make sure you have python 2 and graphvis installed (put graphvis path in this file).
 python visualise.py
 "C:\Program Files (x86)\Graphviz 2.28\bin\neato.exe" -Gcharset=latin1 -Gepsilon=.001 -Tpng graph.dot -o graph.png
